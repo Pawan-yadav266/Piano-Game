5356c98c15174076034cd8ec0c7ed9d9cc41ff2c
DESKTOP-7JCCVRJ\Anuj <raoanujyadav1999@gmail.com>
2022-03-28 15:33:31 +0530

    hello world

---
+++ lib/Note.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Note
+{
+  final int orderNumber;
+  final int line;
+  NoteState state =NoteState.ready;
+   Note(this.orderNumber,this.line);
+}
+enum NoteState{ready,tapped,missed}
